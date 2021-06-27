8f6a00d9b13a7c317bbc9427afbd28d3892bba95
Bondok6 <kirolousbondok6@gmail.com>
2021-06-27 14:22:03 +0200

    form

--- js/main.js
+++ js/main.js
@@ -194,3 +194,29 @@ $(document).ready(function() {
 
 	$('#tag').svg3DTagCloud(settings);
 });
+
+// Contact me Form
+const form = document.getElementById('my-form');
+
+async function handleSubmit(event) {
+	event.preventDefault();
+	const status = document.getElementById('status');
+	const data = new FormData(event.target);
+	fetch(event.target.action, {
+		method: form.method,
+		body: data,
+		headers: {
+			Accept: 'application/json'
+		}
+	})
+		.then((response) => {
+			status.classList.add('success');
+			status.innerHTML = 'Thanks for your submission!';
+			form.reset();
+		})
+		.catch((error) => {
+			status.classList.add('error');
+			status.innerHTML = 'Oops! There was a problem submitting your form';
+		});
+}
+form.addEventListener('submit', handleSubmit);
