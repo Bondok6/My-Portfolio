77a9337b98cb9a31853d03e38fdb45e1833fe69d
Bondok6 <kirolousbondok6@gmail.com>
2022-03-20 09:46:41 +0200

    Add new skills and projects

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-"use strict";
+'use strict';
 
 // SIDE NAVBAR
-const icons = document.querySelectorAll(".icons span");
+const icons = document.querySelectorAll('.icons span');
 
 const clearActive = function () {
   icons.forEach((icon) => {
     icon.style.borderBottom = `none`;
-    icon.classList.remove("active");
+    icon.classList.remove('active');
   });
 };
 
 icons.forEach((icon) => {
-  icon.addEventListener("click", () => {
+  icon.addEventListener('click', () => {
     clearActive();
     icon.style.borderBottom = `2px solid cyan`;
-    icon.classList.add("active");
+    icon.classList.add('active');
   });
 });
 
 // Mouse Hover Animation
-const c = document.getElementById("c");
-const ctx = c.getContext("2d");
+const c = document.getElementById('c');
+const ctx = c.getContext('2d');
 
 const WIDTH = (c.width = window.innerWidth);
 const HEIGHT = (c.height = window.innerHeight);
@@ -62,7 +62,7 @@ Particle.prototype = {
   render: function (ctx) {
     ctx.save();
     ctx.fillStyle =
-      "rgba(" + this.r + ", " + this.g + ", " + this.b + ", " + this.a + ")";
+      'rgba(' + this.r + ', ' + this.g + ', ' + this.b + ', ' + this.a + ')';
     ctx.translate(this.x, this.y);
     ctx.beginPath();
     ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
@@ -108,7 +108,7 @@ function spawn(target) {
   if (++idx >= particles.length) idx = 0;
 }
 
-c.addEventListener("mousemove", function (e) {
+c.addEventListener('mousemove', function (e) {
   let rect = c.getBoundingClientRect();
   mouse.x = e.clientX - rect.left;
   mouse.y = e.clientY - rect.top;
@@ -157,82 +157,83 @@ requestAnimationFrame(function loop() {
 // Skills 3D animations
 $(document).ready(function () {
   const entries = [
-    { label: "HTML" },
-    { label: "CSS" },
-    { label: "JavaScript" },
-    { label: "HTML5" },
-    { label: "CSS3" },
-    { label: "BEM" },
-
-    { label: "Git" },
-    { label: "Github" },
-    { label: "SASS" },
-    { label: "Python" },
-    { label: "React.js" },
-    { label: "OOP" },
-    { label: "JSON" },
-    { label: "ES5/ES6" },
-    { label: "npm" },
-    { label: "Ajax" },
-    { label: "Figma" },
+    { label: 'JavaScript' },
+    { label: 'HTML5' },
+    { label: 'CSS3' },
+    { label: 'Bootstrap5' },
+    { label: 'BEM' },
+    { label: 'Git' },
+    { label: 'Github' },
+    { label: 'SASS' },
+    { label: 'Python3' },
+    { label: 'React.js' },
+    { label: 'Redux' },
+    { label: '@toolkit' },
+    { label: 'OOP' },
+    { label: 'JSON' },
+    { label: 'ES5/ES6' },
+    { label: 'npm' },
+    { label: 'Ajax' },
+    { label: 'Figma' },
+    { label: 'Webpack' },
   ];
 
   const settings = {
     entries: entries,
     width: 640,
     height: 480,
-    raduis: "65%",
+    raduis: '65%',
     raduisMin: 75,
     bgDraw: true,
-    bgColor: "#1d1d1d",
+    bgColor: '#1d1d1d',
     opacityOver: 1.0,
     opacityOut: 0.05,
     opacitySpeed: 6,
     fov: 800,
     speed: 1.2,
-    fontFamily: "Courier, Arial, sans-serif",
-    fontSize: "1.5rem",
-    fontColor: "#08fdd8",
+    fontFamily: 'Courier, Arial, sans-serif',
+    fontSize: '1.5rem',
+    fontColor: '#08fdd8',
   };
 
-  $("#tag").svg3DTagCloud(settings);
+  $('#tag').svg3DTagCloud(settings);
 });
 
 // Contact me Form
-const form = document.getElementById("my-form");
+const form = document.getElementById('my-form');
 
 async function handleSubmit(event) {
   event.preventDefault();
-  const status = document.getElementById("status");
+  const status = document.getElementById('status');
   const data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
     headers: {
-      Accept: "application/json",
+      Accept: 'application/json',
     },
   })
     .then((response) => {
-      status.classList.add("success");
-      status.innerHTML = "Thanks for your submission!";
+      status.classList.add('success');
+      status.innerHTML = 'Thanks for your submission!';
       form.reset();
     })
     .catch((error) => {
-      status.classList.add("error");
-      status.innerHTML = "Oops! There was a problem submitting your form";
+      status.classList.add('error');
+      status.innerHTML = 'Oops! There was a problem submitting your form';
     });
 }
-form.addEventListener("submit", handleSubmit);
+form.addEventListener('submit', handleSubmit);
 // Scroll animation
 const scrollAnimation = function () {
-  window.addEventListener("scroll", () => {
-    const content = document.querySelector(".about__info");
+  window.addEventListener('scroll', () => {
+    const content = document.querySelector('.about__info');
     let contentPos = content.getBoundingClientRect().top;
     let screenPos = window.innerHeight;
     if (contentPos < screenPos) {
-      content.classList.add("about_scroll");
+      content.classList.add('about_scroll');
     } else {
-      content.classList.remove("about_scroll");
+      content.classList.remove('about_scroll');
     }
   });
 };
@@ -240,28 +241,28 @@ scrollAnimation();
 
 // The Sound
 
-const soundBtn = document.querySelector("#sound span");
-const soundLogo = document.querySelector("path");
+const soundBtn = document.querySelector('#sound span');
+const soundLogo = document.querySelector('path');
 const sound = new Audio();
-sound.setAttribute("src", "../audio/sound.mp3");
+sound.setAttribute('src', '../audio/sound.mp3');
 
 const turnOnSound = function () {
-  soundBtn.textContent = "ON";
-  soundBtn.style.color = "#08fdd8";
-  soundLogo.style.fill = "#08fdd8";
+  soundBtn.textContent = 'ON';
+  soundBtn.style.color = '#08fdd8';
+  soundLogo.style.fill = '#08fdd8';
   sound.play();
 };
 
 const turnOffSound = function () {
-  soundBtn.textContent = "OFF";
-  soundBtn.style.color = "#fd1056";
-  soundLogo.style.fill = "#fd1056";
+  soundBtn.textContent = 'OFF';
+  soundBtn.style.color = '#fd1056';
+  soundLogo.style.fill = '#fd1056';
   sound.pause();
 };
 
 const theSound = function () {
-  soundBtn.addEventListener("click", () => {
-    soundBtn.textContent == "ON" ? turnOffSound() : turnOnSound();
+  soundBtn.addEventListener('click', () => {
+    soundBtn.textContent == 'ON' ? turnOffSound() : turnOnSound();
   });
 };
 theSound();
@@ -273,20 +274,20 @@ theSound();
 
 // Disable keyboard
 document.onkeydown = function (e) {
-  if (e.code == "F12") return false;
-  if (e.ctrlKey && e.shiftKey && e.code === "KeyI") return false;
-  if (e.ctrlKey && e.shiftKey && e.code == "KeyJ") return false;
-  if (e.ctrlKey && e.code == "KeyU") return false;
+  if (e.code == 'F12') return false;
+  if (e.ctrlKey && e.shiftKey && e.code === 'KeyI') return false;
+  if (e.ctrlKey && e.shiftKey && e.code == 'KeyJ') return false;
+  if (e.ctrlKey && e.code == 'KeyU') return false;
 };
 
 // Leaflet -> third-party library to display map
 
 const coords = [31.2508271, 32.3189202];
-const map = L.map("map").setView(coords, 13);
+const map = L.map('map').setView(coords, 13);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker(coords).addTo(map).bindPopup("Hi, I am here 👋").openPopup();
+L.marker(coords).addTo(map).bindPopup('Hi, I am here 👋').openPopup();
